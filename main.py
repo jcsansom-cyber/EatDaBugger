e11bb719852ed3df6c777f4cc3cf2a2eb4e66a3c
Gillian Zech <ginger01@email.unc.edu>
2022-02-19 14:24:02 -0500

    still working through the tutorial

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pygame
 import os
 
 WIDTH, HEIGHT = 900, 500
+CENTER = (450, 250)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
-player_img = pygame.image.load(os.path.join('Assets', ''))
+PLAYER_IMG = pygame.image.load(os.path.join('Assets', 'UFo.png'))
+PLAYER_IMG = pygame.transform.scale(PLAYER_IMG, (500, 500))
+PLAYER_IMG = pygame.image.rotate(PLAYER_IMG, 0)
+BULLET_IMG = pygame.image.load(os.path.join('Assets', 'UFo.png'))
+BUG1_IMG = pygame.image.load(os.path.join('Assets', 'UFo.png'))
+BUG2_IMG = pygame.image.load(os.path.join('Assets', 'UFo.png'))
+BUG3_IMG = pygame.image.load(os.path.join('Assets', 'UFo.png'))
 pygame.display.set_caption("Pearl Shooter")
 
 
 
 def draw_window():
     WIN.fill((0, 0, 0))
+    WIN.blit(PLAYER_IMG, CENTER)
     pygame.display.update()
 
 
