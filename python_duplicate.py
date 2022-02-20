271a35e589f627fbe1c4f9af7f3a95e9f97f0778
Gillian Zech <ginger01@email.unc.edu>
2022-02-19 21:34:46 -0500

    pushing what I got

--- python_duplicate.py
+++ python_duplicate.py
@@ -28,16 +28,14 @@ BUG3_IMG = pygame.transform.scale(BUG3_IMG, (BAD_BUG_WIDTH, BAD_BUG_HEIGHT))
 PLAYER_HIT = pygame.USEREVENT + 1
 pygame.display.set_caption("Eat Da Bugger")
 
-def draw_bugs(ladybugs, bad_bugs, bad_bugs_eaten, bad_bugs_dist):
+def draw_window(player, ladybugs, bad_bugs, bad_bugs_eaten, bad_bugs_dist):
+    WIN.fill((50, 50, 50))
+    # WIN.blit(, (player.x, player.y))
     for lady in ladybugs:
         WIN.blit(BUG1_IMG, (lady.x, lady.y))
-    for baddie in bad_bugs_dist:
-        WIN.blit(bad_bugs_dist[bad_bugs.index(baddie)], (baddie.x, baddie.y))
-
-def draw_window(player):
-    WIN.fill((0, 0, 0))
-    # WIN.blit(, (player.x, player.y))
-    
+    for baddie in bad_bugs:
+        if not bad_bugs_eaten[bad_bugs.index(baddie)]:
+            WIN.blit(bad_bugs_dist[bad_bugs.index(baddie)], (baddie.x, baddie.y))
     WIN.blit(PLAYER_IMG, (player.x, player.y))
     # WIN.blit("Points", (player.x, player.y))
 
@@ -63,10 +61,10 @@ def main():
         pygame.Rect(random.randrange(int(BAD_BUG_WIDTH/2), WIDTH-int(BAD_BUG_WIDTH/2)), 370, BAD_BUG_WIDTH, BAD_BUG_HEIGHT),
     ]
     bad_bugs_eaten = [False, False, False, False, False, False, False, False]
-    bad_bugs_dist = [BUG2_IMG, BUG2_IMG, BUG2_IMG, BUG2_IMG, BUG2_IMG, BUG2_IMG, BUG2_IMG, BUG2_IMG]
+    bad_bugs_dist = [BUG2_IMG, BUG3_IMG, BUG2_IMG, BUG3_IMG, BUG2_IMG, BUG3_IMG, BUG2_IMG, BUG3_IMG]
     for baddie in bad_bugs_dist:
         a = random.choice("12");
-        if a == "1":
+        if a == "2":
             baddie = BUG3_IMG;
     ladybugs = [
         pygame.Rect(random.randrange(int(LADYBUG_WIDTH/2), WIDTH-int(LADYBUG_WIDTH/2)), 90, LADYBUG_WIDTH, LADYBUG_HEIGHT),
@@ -81,25 +79,27 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        # player.x += 1
+
         
         keys_pressed = pygame.key.get_pressed() # checks what keys are currently being pressed
-        if keys_pressed[pygame.K_LEFT] and player.x - VELOCITY > 0:
+        if (keys_pressed[pygame.K_LEFT] or keys_pressed[pygame.K_a]) and player.x - VELOCITY > 0:
             player.x -= VELOCITY;
-        if keys_pressed[pygame.K_RIGHT] and player.x + VELOCITY + PLAYER_WIDTH < WIDTH:
+        if (keys_pressed[pygame.K_RIGHT] or keys_pressed[pygame.K_d]) and player.x + VELOCITY + PLAYER_WIDTH < WIDTH:
             player.x += VELOCITY;
-        if keys_pressed[pygame.K_UP] and player.y - VELOCITY > 0:
+        if (keys_pressed[pygame.K_UP] or keys_pressed[pygame.K_w]) and player.y - VELOCITY > 0:
             player.y -= VELOCITY;
-        if keys_pressed[pygame.K_DOWN] and player.y + VELOCITY + PLAYER_HEIGHT < HEIGHT:
+        if (keys_pressed[pygame.K_DOWN] or keys_pressed[pygame.K_s]) and player.y + VELOCITY + PLAYER_HEIGHT < HEIGHT:
             player.y += VELOCITY;
         for bug in bad_bugs:
             if player.colliderect(bug):
                 x = bad_bugs.index(bug)
                 bad_bugs_eaten[x] = True
                 # points+=1
-
-        draw_window(player)
-        draw_bugs(ladybugs, bad_bugs, bad_bugs_eaten, bad_bugs_dist)
+        running = False # Made false only to be made true again as long as there are still uneaten bugs
+        for condition in bad_bugs_eaten:
+            if not condition:
+                running = True;
+        draw_window(player, ladybugs, bad_bugs, bad_bugs_eaten, bad_bugs_dist)
             
     pygame.quit()
 
